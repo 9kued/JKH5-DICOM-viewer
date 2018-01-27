23f9b98c2f1b7d0b935b7cdc476046518faee5e1
yuanxuegui <yuanxuegui@163.com>
2018-01-27 18:34:27 +0800

    h5dicom

--- build-pro.sh
+++ build-pro.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-METEOR_ALLOW_SUPERUSER=true meteor-build-client /opt/data/viewer.jiukangguoji.cn/dwv -p "" -u http://viewer.jiukangguoji.cn/dwv -s ./config/public.json
+METEOR_ALLOW_SUPERUSER=true meteor-build-client /opt/data/viewer.jiukangguoji.cn/h5dicom -p "" -u http://viewer.jiukangguoji.cn/h5dicom -s ./config/public.json
